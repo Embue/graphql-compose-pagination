ec05281d091d23b0dff3e353ab42d16a34de5ac1
Peter Pavlovich <peter.pavlovich@embue.com>
2022-08-23 11:58:47 -0400

    try adding the lib dir

---
+++ lib/index.d.ts
@@ -0,0 +1,6 @@
+import { preparePaginationResolver } from './pagination';
+import { composeWithPagination } from './composeWithPagination';
+import { preparePaginationTC } from './types';
+export { composeWithPagination, preparePaginationResolver, preparePaginationTC };
+export type { PaginationResolverOpts, PaginationTArgs, PaginationType, PaginationInfoType, } from './pagination';
+//# sourceMappingURL=index.d.ts.map
